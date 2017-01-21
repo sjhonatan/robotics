4dc2e9e326ad8a88710a2d58ccfbb842a6226157
Jhonatan da Silva <jhonatan.s04@aluno.ifsc.edu.br>
2017-01-21 20:17:10 -0200

    update

--- python/Cap1/codes/homogenea.py
+++ python/Cap1/codes/homogenea.py
@@ -1,11 +1,10 @@
 import numpy as np
 
-T = np.matrix('1 0 0 0; 0 0 -1 3; 0 1 0 1; 0 0 0 1')
+T = np.matrix('1 0 0 0; 0 0 -1 3; 0 1 0 2; 0 0 0 1')
 
 Pb = np.matrix('0;1;1;1')
 
 Pa = np.dot(T,Pb)
 
 Pa = Pa[0:3]
-
 
